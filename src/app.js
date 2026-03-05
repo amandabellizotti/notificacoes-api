cf537a7adf8961c3604d77269f90a7d54787560a
Amanda <amanda.bellizotti@aluno.senai.br>
2026-03-05 10:50:46 -0300

    add conteudo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 // src/app.js
 const express = require("express");
 const app = express();
+
 // Middleware para ler JSON no body das requisições
 app.use(express.json());
-// Rota de teste — vamos remover depois
+
+// Importando as rotas de eventos
+const eventoRoutes = require("./routes/eventoRoutes");
+
+// Usar rotas com prefixo
+app.use("/eventos", eventoRoutes);
+
+// Rota raiz (informativa)
 app.get("/", (req, res) => {
-res.json({ mensagem: "API de Notificações funcionando! 🚀" });
+    res.json({
+        mensagem: "API de Notificações",
+        rotas: {
+            eventos: "/eventos",
+        },
+    });
 });
 module.exports = app;
